41bc60e97e1300b660b990bb99760ec1a4d204a7
Quang VT <36344264+TheSylph98@users.noreply.github.com>
2020-06-04 00:09:59 +0700

    new branch

--- word_similar.py
+++ word_similar.py
@@ -1,6 +1,7 @@
 from pyvi import ViTokenizer
 from gensim.models.keyedvectors import KeyedVectors
 
+
 class Word_Similar():
     def __init__(self, path_model="./word2vec/baomoi.window2.vn.model.bin", N=3, path_sw='vi_stopwords.txt'):
         self.model = KeyedVectors.load_word2vec_format(path_model, binary=True)
@@ -19,7 +20,7 @@ class Word_Similar():
 
     def get_stopword(self, paths):
         stopword = []
-        with open(paths, 'r') as files:
+        with open(paths, 'r',  encoding="utf8") as files:
             for word in files:
                 stopword.append(word.replace("\n", ''))
         return stopword
@@ -29,9 +30,10 @@ class Word_Similar():
         words = self.filter_stopwords(sentence, stop=False)
         word_similar = []
         for word in words:
-            word_similar.append([(word,1.0)])
+            word_similar.append([(word, 1.0)])
             try:
-                word_similar.append(self.model.wv.most_similar(positive=[word], topn=self.number_top))
+                word_similar.append(self.model.wv.most_similar(
+                    positive=[word], topn=self.number_top))
             except:
                 pass
         text = ''
@@ -40,10 +42,10 @@ class Word_Similar():
             for word in words:
                 if word[1] > 0.7:
                     text += "'" + word[0] + "' "
-        
+
         return text[:-1]
+
 
 # ws = Word_Similar()
 
 # print(ws.find_word_similar("Phòng"))
-    
